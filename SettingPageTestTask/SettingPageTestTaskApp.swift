d94b31ce84c266158ab8357039fc3cde670d0029
maxKokhaniuk <maxkohanuk69@gmail.com>
2025-09-03 13:19:36 +0300

    Initial Commit

---
+++ SettingPageTestTask/SettingPageTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingPageTestTaskApp.swift
+//  SettingPageTestTask
+//
+//  Created by MaxK on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SettingPageTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
